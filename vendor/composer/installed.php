6bca3904874c481da64f12adabaabafb97fef2fd
leafingthru <133003450+leafingthru@users.noreply.github.com>
2025-01-14 11:04:00 +0800

    included package-lock.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '997fd6547c0a15e7f0c3f18ed4a7f0b6d0fb374b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '997fd6547c0a15e7f0c3f18ed4a7f0b6d0fb374b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twig/twig' => array(
+            'pretty_version' => 'v1.13.2',
+            'version' => '1.13.2.0',
+            'reference' => '6d6a1009427d1f398c9d40904147bf9f723d5755',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twig/twig',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
